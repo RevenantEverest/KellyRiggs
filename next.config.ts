d4a86f2362717fd9eb53dd23cbfc7809788aa949
Stefan <stefischer@mail.com>
2026-01-05 10:55:48 -0500

    Remove allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', "https://c0072ee1b4d1.ngrok-free.app"]
 };
 
 export default nextConfig;
